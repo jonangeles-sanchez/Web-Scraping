1e7543d4acbd5d14c8acf59a52942ffa8b400ccf
Jonathan Angeles <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-09-25 12:36:26 -0400

    Web scrape method for address
    Based off what I was able to scrape using the price method, i decided to use the same method to scrape the first line and second line of property address. Now working on beds, etc...

--- theWebScraper.py
+++ theWebScraper.py
@@ -9,3 +9,8 @@ soup=BeautifulSoup(c,"html.parser")
 all=soup.find_all("div", {"class":"propertyRow"})
 
 all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
+
+for item in all:
+    print(item.find ("h4",{"class","propPrice"}).text.replace("\n","").replace(" ",""))
+    print(item.find_all("span",{"class","propAddressCollapse"})[0])
+    print(item.find_all("span",{"class","propAddressCollapse"})[1])
