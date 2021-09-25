8e0bb634908cc092c06bb875fa21fbc5591612a2
Jonathan Angeles <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-09-25 12:28:02 -0400

    Added price scrape method
    Price scrape from the website method has been added. Website would return the price but with extra "\n", so I included code to remove that with empty space. And then more code to replace whitespace with nothing to just return price text only.

--- theWebScraper.py
+++ theWebScraper.py
@@ -5,4 +5,7 @@ r=request.get("https://www.century21.com/real-estate/mars-hill-nc/LCNCMARSHILL/"
 c=r.content
 
 soup=BeautifulSoup(c,"html.parser")
-print(soup.prettify())
+
+all=soup.find_all("div", {"class":"propertyRow"})
+
+all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
